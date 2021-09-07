917cf05ac2e186013fd9420207a7e93881fe443f
Patrick Santino <pet.santino@gmail.com>
2021-09-07 08:48:34 +0700

    Handle common api exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +41,50 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+            if ($exception instanceof AuthenticationException) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unauthenticated',
+                    'errors' => [
+                        'Unauthenticated'
+                    ]
+                ], 401);
+            }
+
+            if ($exception instanceof AuthorizationException) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'This action is unauthorized.',
+                    'errors' => [
+                        'This action is unauthorized.'
+                    ]
+                ], 403);
+            }
+
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Entry for ' . str_replace('App\\Models\\', '', $exception->getModel()) . ' not found',
+                    'errors' => [
+                        'Entry for ' . str_replace('App\\Models\\', '', $exception->getModel()) . ' not found'
+                    ]
+                ], 404);
+            }
+
+            if ($exception instanceof ValidationException) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'The given data was invalid.',
+                    'errors' => collect($exception->errors())->flatten()
+                ], 422);
+            }
+        }
+
+        return parent::render($request, $exception);
     }
 }
