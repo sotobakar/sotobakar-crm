2f73aadbf71e11ddef6fbd6d673c12cfb2583963
Patrick Santino <pet.santino@gmail.com>
2021-09-07 21:38:37 +0700

    Added Project model updated event

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        if ($project->projectStatus->name == 'Completed') {
+            $project->completed_at = now();
+            $project->saveQuietly();
+        } else {
+            $project->completed_at = null;
+            $project->saveQuietly();
+        }
+    }
+
+    /**
+     * Handle the Project "deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "restored" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "force deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
