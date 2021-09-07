f8cea0fcd5f06c18820581b39e833482a9b77092
Patrick Santino <pet.santino@gmail.com>
2021-09-07 21:36:34 +0700

    Task CRUD

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\StoreTaskRequest;
+use App\Http\Requests\Api\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Project;
 use App\Models\Task;
@@ -11,23 +13,35 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     /**
+     * Create the controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class, 'task');
+    }
+
+    /**
      * Display a listing of the resource.
-     * @param  \App\Models\Project  $project
+     *
      * @return \Illuminate\Http\Response
      */
     public function index(Project $project)
     {
+        return TaskResource::collection($project->tasks->load('taskStatus'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
     public function store(StoreTaskRequest $request, Project $project)
     {
+        $task = $project->tasks()->create($request->validated())->load(['taskStatus']);
+        return (new TaskResource($task))->additional(['message' => 'Task created successfully']);
     }
 
     /**
@@ -38,7 +52,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        //
+        return new TaskResource($task->load(['taskStatus']));
     }
 
     /**
@@ -48,9 +62,13 @@ class TaskController extends Controller
      * @param  \App\Models\Task  $task
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Task $task)
+    public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $data = $request->validated();
+        $data['project_id'] = $task->project->id;
+        $task->update($data);
+
+        return (new TaskResource($task->refresh()->loadMissing(['project', 'taskStatus'])))->additional(['message' => 'Task updated successfully']);
     }
 
     /**
@@ -61,6 +79,9 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->json([
+            'message' => 'Task deleted successfully'
+        ], 200);
     }
 }
---
+++ app/Http/Requests/Api/UpdateTaskRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'task_status_id' => 'required|exists:task_statuses,id',
+            'description' => 'string'
+        ];
+    }
+}
